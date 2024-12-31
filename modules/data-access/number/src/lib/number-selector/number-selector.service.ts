4f015cf132230b533f7dd715706e2542960335f6
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-31 14:07:44 -0300

    feat(home): add lib

--- modules/data-access/number/src/lib/number-selector/number-selector.service.ts
+++ modules/data-access/number/src/lib/number-selector/number-selector.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NumberSelectorService {
+  private selectedNumbers: number[] = [];
 
-  constructor() { }
+  selectNumber(num: number): void {
+    if (this.selectedNumbers.includes(num)) {
+      this.selectedNumbers = this.selectedNumbers.filter((n) => n !== num);
+    } else if (this.selectedNumbers.length < 15) {
+      this.selectedNumbers.push(num);
+    }
+  }
+
+  getSelectedNumbers(): number[] {
+    return [...this.selectedNumbers];
+  }
+
+  resetSelection(): void {
+    this.selectedNumbers = [];
+  }
+
+  generateRandomSelection(): void {
+    this.resetSelection();
+    while (this.selectedNumbers.length < 6) {
+      const randomNum = Math.floor(Math.random() * 60) + 1;
+      if (!this.selectedNumbers.includes(randomNum)) {
+        this.selectedNumbers.push(randomNum);
+      }
+    }
+  }
 }
